faa3b87836c313382cf12332a3b98cdb647e69a1
Yul Eberlein <yul.eberlein@rwth-aachen.de>
2020-05-04 13:20:54 +0200

    Button click script added

---
+++ Assets/Scripts/LocationButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class LocationButton : MonoBehaviour, IMixedRealityPointerHandler
+{
+    public TextMeshPro textMesh;
+
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        Debug.Log(textMesh.text);
+    }
+
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+    }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+    }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
