1bad72fa149187839230775e82d44d76ce827569
Yul Eberlein <yul.eberlein@rwth-aachen.de>
2020-05-04 10:42:30 +0200

    WetherWidget Prefab added

---
+++ Assets/WeatherApp/Scripts/WeatherWidget/RainController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainController : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem rainSystem;
+
+    private ParticleSystem.EmissionModule emissionModule;
+
+    private void Awake()
+    {
+        EnsureModule();
+    }
+
+    private void EnsureModule()
+    {
+        emissionModule = rainSystem.emission;
+    }
+    public bool RainEnabled
+    {
+        get
+        {
+            EnsureModule();
+            return emissionModule.enabled;
+        }
+        set
+        {
+            EnsureModule();
+            emissionModule.enabled = value;
+        }
+    }
+
+    public float RainRate
+    {
+        get
+        {
+            EnsureModule();
+            return emissionModule.rateOverTime.constant;
+        }
+        set
+        {
+            EnsureModule();
+            emissionModule.rateOverTime = value;
+        }
+    }
+}
