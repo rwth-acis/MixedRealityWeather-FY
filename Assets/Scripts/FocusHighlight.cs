ce920b0e38d529296954e6af09d8d27c3454363f
Yul Eberlein <yul.eberlein@rwth-aachen.de>
2020-05-04 13:07:42 +0200

    FokusHIghlight scripted

---
+++ Assets/Scripts/FocusHighlight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class FocusHighlight : MonoBehaviour, IMixedRealityFocusHandler
+{
+    private Color defaultColor;
+    public Color focusColor;
+    public Renderer backgroundRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultColor = backgroundRenderer.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnFocusEnter(FocusEventData eventData)
+    {
+        backgroundRenderer.material.color = focusColor;
+    }
+
+    public void OnFocusExit(FocusEventData eventData)
+    {
+        backgroundRenderer.material.color = defaultColor;
+    }
+}
