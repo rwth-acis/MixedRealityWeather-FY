f88651e94ccc0cbe02b0e364ca7da20f36440bf9
Felix Voigtländer <voigtlaender.felix01@googlemail.com>
2020-05-07 12:32:32 +0200

    Anbindung zu OpenWeatherMap implementieren
    Fixed #7
    Exercise 2.2g
    Added WeatherQuery, WeatherData (and sub Classes)
    
    Manages Fetchrequests to OpenWeatherMap

---
+++ Assets/Scripts/WeatherData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class WeatherData 
+{
+    public List<WeatherState> weather;
+    public ClimateData main;
+    public int id;
+    public string name;
+}
+
+[Serializable]
+public class WeatherState
+{
+    public int id;
+    public string main;
+}
+
+[Serializable]
+public class ClimateData
+{
+    public float temp;
+    public float feels_like;
+    public float temp_min;
+    public float temp_max;
+    public float pressure;
+    public float humidity;
+
+}
---
+++ Assets/Scripts/WeatherQuery.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System.Threading.Tasks;
+using System.Net;
+using System;
+using System.IO;
+
+public class WeatherQuery : MonoBehaviour
+{
+    //Task: 2.2g
+    //By Felix Voigtländer 07.05.2020
+    //Look up Links: 
+    // Async/Rest/await: https://docs.microsoft.com/de-de/dotnet/csharp/programming-guide/concepts/async/
+    // WeatherWebsite: https://openweathermap.org/current
+    // !WeatherWebsite Tutorial: https://www.red-gate.com/simple-talk/dotnet/c-programming/calling-restful-apis-unity3d/
+
+    public WeatherData weatherData;
+    private async Task Start()
+    {
+        print("Weather test");
+        weatherData = await FetchWeatherData("Sydney");
+    }
+
+    public async Task<WeatherData> FetchWeatherData(string cityName)
+    {
+        print("Starting to Fetch");
+        // Request Information
+        string apiKey = "16fb128280d16cd1acdeed497976a523";
+
+        //HTTP Fetch
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", cityName, apiKey));
+        HttpWebResponse response = (HttpWebResponse)(await request.GetResponseAsync());
+
+        //Parse to Json String
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string jsonResponse = reader.ReadToEnd();
+
+        // Parse from Json string to WeatherData
+        WeatherData data = JsonUtility.FromJson<WeatherData>(jsonResponse);
+
+        //Return Fetched WeatherData
+        return data;
+    }
+}
